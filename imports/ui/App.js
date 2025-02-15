ebac0f2593617cdac6f6b5d83ba0ff7bf993a3b1
Gnaneswari <gnlolugu@iu.edu>
2025-02-15 13:24:25 -0500

    filtering tasks by adding reactive components

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,13 +1,42 @@
 import { Template } from 'meteor/templating';
 import { TasksCollection } from '../api/TasksCollection';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import './App.html';
 import './Task.js';
 
-Template.mainContainer.helpers({
-  tasks() {
-    return TasksCollection.find({}, { sort: { createdAt: -1 } });
-  },
+const HIDE_COMPLETED_STRING = "hideCompleted";
+Template.mainContainer.onCreated(function mainContainerOnCreated() {
+  this.state = new ReactiveDict();
 });
+
+Template.mainContainer.helpers({
+    tasks() {
+      const instance = Template.instance();
+      const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+  
+      const hideCompletedFilter = { isChecked: { $ne: true } };
+  
+      return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+        sort: { createdAt: -1 },
+      }).fetch();
+    },
+    hideCompleted() {
+      return Template.instance().state.get(HIDE_COMPLETED_STRING);
+    },
+    incompleteCount() {
+        const incompleteTasksCount = TasksCollection.find({ isChecked: { $ne: true } }).count();
+        return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
+      },
+  });
+
+Template.mainContainer.events({
+    "click #hide-completed-button"(event, instance) {
+      const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+      instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+    }
+  });
+
 
 Template.form.events({
   'submit .task-form'(event) {
